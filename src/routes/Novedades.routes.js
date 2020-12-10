79ec8bf3072a874784f5c9272a23bc69b9cd6f67
Desarrollo <onlin1992@gmail.com>
2020-12-10 01:48:48 -0500

    listo produccion version 1.0

--- src/routes/Novedades.routes.js
+++ src/routes/Novedades.routes.js
@@ -1,6 +1,9 @@
-const { Router } = require("express");
-const NovededesCtrl = require("../controllers/Novedades.controller");
-
-const router = Router();
-router.get("/", NovededesCtrl.consultar).get("/*", NovededesCtrl.error);
-module.exports = router;
+const { Router } = require('express')
+const NovededesCtrl = require('../controllers/Novedades.controller')
+const { isAuth } = require('../middlewares/acceso')
+const router = Router()
+router
+  .post('/guardar', isAuth, NovededesCtrl.guardar)
+  .get('/', NovededesCtrl.consultar)
+  .get('/*', NovededesCtrl.error)
+module.exports = router
