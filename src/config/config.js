108916539ede3cc928ea77e412040fd5c8cdcdae
Desarrollo <onlin1992@gmail.com>
2020-12-11 06:50:19 -0500

    cambios para mejorar las consultas

--- src/config/config.js
+++ src/config/config.js
@@ -12,13 +12,13 @@ module.exports = {
   USER_FIREBIRD: process.env.USER_FIREBIRD || "sysdba",
   PASS_FIREBIRD: process.env.PASS_FIREBIRD || "masterkey",
   SIIGO_USERNAME:
-    process.env.SIIGO_USERNAME || "EMPRESA2CAPACITACION\\empresa2@apionmicrosoft.com",
-  SIIGO_PASSWORD: process.env.SIIGO_USERNAME || "s112pempresa2#",
+    process.env.SIIGO_USERNAME || "GRUPOP28651@apionmicrosoft.com",
+  SIIGO_PASSWORD: process.env.SIIGO_USERNAME || "%vi12m8=dM",
   SIIGO_SUSCRIPTION:
-    process.env.SIIGO_SUSCRIPTION || "184165d1878e45f2910bc99e870ac781",
+    process.env.SIIGO_SUSCRIPTION || "b838c206d9674e9e95b009f74e92a0e3",
   SIIGO_CONEXION: {
     SIIGO_USERNAME: "GRUPOP28651@apionmicrosoft.com",
-    SIIGO_PASSWORD: "s112pempresa2#",
+    SIIGO_PASSWORD: "%vi12m8=dM",
     SIIGO_SUSCRIPTION: "b838c206d9674e9e95b009f74e92a0e3",
   },
   SIIGO_PARAMETROS: {
